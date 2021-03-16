6fdf199ba6668337c99cb0413db2f48669911e16
Carl <carl.agbenyega@gmail.com>
2021-03-16 17:28:14 -0600

    edited dashboard, added new components for org displays

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,17 +4,31 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./../component_styles/dashboard.styles.css";
 
 import LargeButton from "./large-button";
+import OrgTableList from "./org-table-list";
 
 const Dashboard = () => {
   return (
     <Container fluid>
       <h1>Dashboard</h1>
-      <Row>
+      <Row className="section">
         <Col>
           <LargeButton title="Add Organization" url="/organisations" />
         </Col>
         <Col>
           <LargeButton title="Add Event" url="/events" />
+        </Col>
+      </Row>
+      <Row className="section">
+        <Col>
+          <h4>Clubs & Organizations</h4>
+          <OrgTableList />
+        </Col>
+        <Col>
+          <h4>Events</h4>
+          <strong>Upcoming</strong>
+          <Row></Row>
+          <strong>Past</strong>
+          <Row></Row>
         </Col>
       </Row>
     </Container>
