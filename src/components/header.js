33f8a849e4f98fe56b83404de31776f6db2ab369
Carl <carl.agbenyega@gmail.com>
2021-03-19 22:28:47 -0600

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -13,10 +13,10 @@ const Header = () => {
           <Nav.Link href="#link">Organisations</Nav.Link>
           <Nav.Link href="#link">Events</Nav.Link>
         </Nav>
-        <Form inline>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
-        </Form>
+        </Form> */}
       </Navbar.Collapse>
     </Navbar>
   );
